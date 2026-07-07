833ac1f167a0cecec0e702ab57750c648818eed5
Sabu Siyad <hello@ssiyad.com>
2026-07-07 21:28:33 +0530

    chore: change vault access log id to uuid

--- vault/vault/doctype/vault_access_log/vault_access_log.py
+++ vault/vault/doctype/vault_access_log/vault_access_log.py
@@ -5,4 +5,19 @@ from frappe.model.document import Document
 
 
 class VaultAccessLog(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		action: DF.Literal["read", "write", "list", "reveal"]
+		node: DF.Data | None
+		result: DF.Literal["allow", "deny"]
+		secret_key: DF.Data | None
+		source_ip: DF.Data | None
+	# end: auto-generated types
+
 	pass
