21aa623cdf95bfe5ea4f273f394acd9d2dc5216a
Sabu Siyad <hello@ssiyad.com>
2026-07-08 10:29:34 +0530

    chore: add missing files for vault_policy

---
+++ vault/vault/doctype/vault_policy/vault_policy.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, ssiyad and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vault Policy", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- vault/vault/doctype/vault_policy/vault_policy.py
+++ vault/vault/doctype/vault_policy/vault_policy.py
@@ -6,6 +6,23 @@ from frappe.model.document import Document
 
 
 class VaultPolicy(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		can_list: DF.Check
+		can_read: DF.Check
+		can_write: DF.Check
+		enabled: DF.Check
+		ip_allowlist: DF.SmallText | None
+		key_pattern: DF.Data
+		node: DF.Link
+	# end: auto-generated types
+
 	def validate(self):
 		if not (self.can_read or self.can_write or self.can_list):
 			frappe.throw("A policy must allow at least one action (read, write or list).")
